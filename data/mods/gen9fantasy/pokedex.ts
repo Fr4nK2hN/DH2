a01d323d30572454fc4c923df6fc3ad472af47e5
Fr4nK2hN <729046112@qq.com>
2025-03-29 21:56:35 +0800

    add format "fantasy"

---
+++ data/mods/gen9fantasy/pokedex.ts
@@ -0,0 +1,10 @@
+export const Pokedex: { [k: string]: ModdedSpeciesData } = {
+	garchompfantasy: {
+		inherit: true,
+		name: "Garchomp-Fantasy",
+		num: 445,
+		types: ["Dragon", "Ground"],
+		baseStats: { hp: 108, atk: 140, def: 95, spa: 80, spd: 85, spe: 112 },
+		abilities: { 0: "Rough Skin", H: "Sand Force" },
+	},
+};
