463c52a2b8a11c6f5a265230660df9f1f7426d6c
Fr4nK2hN <729046112@qq.com>
2025-03-30 15:10:49 +0800

    test

--- data/mods/gen9fantasy/scripts.ts
+++ data/mods/gen9fantasy/scripts.ts
@@ -1,3 +1,9 @@
-export const Scripts: BattleScriptsData = {
-	gen: 9,
+export const Scripts: { [k: string]: ModdedBattleScriptsData } = {
+	inherit: "gen9",
+	teambuilderConfig: {
+		// for micrometas to only show custom tiers
+		excludeStandardTiers: true,
+		// only to specify the order of custom tiers
+		customTiers: ["IDK", "EF"],
+	},
 };
